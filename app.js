e005daf8a3de86cf057b2da4a18641403f6f060b
rottencarrot33 <122114827+rottencarrot33@users.noreply.github.com>
2023-05-23 21:14:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    // Get the query parameter from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const targetSlide = urlParams.get('slide');
+
+    // Activate the specified slide
+    if (targetSlide) {
+        const carousel = document.getElementById('rotating-carousel');
+        const carouselInstance = new bootstrap.Carousel(carousel);
+        carouselInstance.to(parseInt(targetSlide) - 1);
+    }
+});
+
+
+
+// Set the timeout for refreshing the page 
+function refreshPage() {
+    window.location.reload();
+}
+
+setTimeout(refreshPage, 30 * 60 * 1000);
+
+
+
+// Remove cars slider on mobile
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth < 500) {
+        var carouselItem = document.getElementById('carousel-item-mobile');
+        carouselItem.parentNode.removeChild(carouselItem);
+    }
+});
+
+
+
+
+
+
---
+++ fs.js
@@ -0,0 +1,20 @@
+
+setTimeout(function () {
+    window.location.href = 'https://rottencarrot33.github.io/kassandra-exc/';
+}, 3 * 60 * 1000);
+
+
+
+// Add 'Bus stop' on <li>
+
+let ulElement = document.querySelector('ul');
+
+let liElement = ulElement.querySelector('li');
+
+liElement.textContent += ' (Bus stop)';
+
+
+
+
+
+
